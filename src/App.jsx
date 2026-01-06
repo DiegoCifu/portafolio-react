9b10526187bd0a38ec2bc0c3f1e799be9f7da2f6
DiegoCifu <diegocifuentesposada@gmail.com>
2026-01-05 23:38:48 -0500

    Actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
@@ -7,18 +7,16 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <> {}
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/proyectos" element={<Projects />} />
         <Route path="/sobre-mi" element={<About />} />
         <Route path="/contacto" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
 export default App;
-
